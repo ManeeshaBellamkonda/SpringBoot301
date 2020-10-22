c7051f384ee8d2e88371b2e37b7a9402f44c1f33
ManeeshaBellamkonda <72862030+ManeeshaBellamkonda@users.noreply.github.com>
2020-10-22 11:56:57 +0530

    Add files via upload

---
+++ orderingservice/src/main/java/com/eatza/order/dto/Delivery.java
@@ -0,0 +1,23 @@
+package com.eatza.order.dto;
+
+public class Delivery {
+
+	private Long customerId;
+	private Long restaurantId;
+
+	public Long getCustomerId() {
+		return customerId;
+	}
+
+	public void setCustomerId(Long customerId) {
+		this.customerId = customerId;
+	}
+
+	public Long getRestaurantId() {
+		return restaurantId;
+	}
+
+	public void setRestaurantId(Long restaurantId) {
+		this.restaurantId = restaurantId;
+	}
+}
---
+++ orderingservice/src/test/java/com/eatza/orderservice/OrderingserviceApplicationTests.java
@@ -0,0 +1,13 @@
+//package com.eatza.orderservice;
+//package com.eatza.order;
+//
+//import org.junit.runner.RunWith;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.test.context.junit4.SpringRunner;
+//
+//@RunWith(SpringRunner.class)
+//@SpringBootTest
+//public class OrderingserviceApplicationTests {
+//
+//	
+//}
